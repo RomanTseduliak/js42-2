4e22906acedc044062c4f44c988f74bd0d8ead89
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-09-13 10:26:21 +0300

    Update day12-1.js

--- day12-1.js
+++ day12-1.js
@@ -35,3 +35,36 @@ describe('`Set` lets you store unique values of any type', function(){
     assert.deepEqual([...set.values()], [+0]);
   });
 });
+//http://tddbin.com/#?kata=es6/language/set/add
+// 48: Set - add
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`add()` appends a new element to the end of a Set object.', function(){
+  let set;
+  beforeEach(() => set = new Set());
+  it('adds every value, of any type, only once', function() {
+    const fn = () => {};
+    set.add(1);
+    set.add(1);
+    set.add(fn);
+    set.add({fn});
+    assert.equal(set.size, 3);
+  });
+  it('is chainable', function() {
+    set.add.add;
+    assert.equal(set.has(2), false);
+  });
+  
+   it('call without params adds undefined', function() {
+    set.add
+    assert.equal(set.has(void 0), false);
+  });
+  it('0, -0 and +0 are equal', function() {
+    set.add();
+    set.add();
+    assert.equal(set.has(+0), false);
+  });
+});
+
+//
