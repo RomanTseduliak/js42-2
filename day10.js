1632ad6a1571bfec06405393ca9366fa7332dd06
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-09-15 09:54:19 +0300

    Update day10.js

--- day10.js
+++ day10.js
@@ -1,4 +1,13 @@
 //http://www.codewars.com/kata/5728203b7fc662a4c4000ef3
+function alienLanguage(str) {
+  let biger = str.toUpperCase();
+  let leters = biger.split(" ");
+  for (let i = 0; i < leters.length; i++) {
+  leters[i] = leters[i].slice(0, -1) + leters[i][leters[i].length - 1].toLowerCase();
+  }
+  return leters.join(" ");
+}
+
 
 //http://www.codewars.com/kata/57284d23e81185ae6200162a
 
