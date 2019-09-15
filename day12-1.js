247be59a22e2da22a8c5fdddcefd71f40acd27ff
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-09-15 09:55:11 +0300

    Update day12-1.js

--- day12-1.js
+++ day12-1.js
@@ -118,3 +118,28 @@ describe('`set.delete()` deletes an element from a set', function(){
 //http://tddbin.com/#?kata=es6/language/set/api
 
 //http://tddbin.com/#?kata=es6/language/set/clear
+describe('`clear()` removes all elements from a Set object.', function(){
+  let set;
+  beforeEach(() => set = new Set());
+  it('`set.size` becomes 0', function() {
+    set.add('one').add('one');
+    set.clear('one');
+    var expectedSize =false;
+    assert.equal(set.size, expectedSize);
+  });
+  it('the iterator `set.entries()` will not contain any items', function() {
+    set.add('one').add(2);
+    set.clear();
+    const {done} = set.entries().next();
+    assert.equal(done, true);
+  });
+  it('any call to `set.has()` returns false', function() {
+    set.add('one').add(2);
+    set.clear();
+    assert.deepEqual(set.has(2), false);
+  });
+  it('returns `undefined`', function() {
+    var expectedReturn ;
+    assert.equal(set.clear(), expectedReturn);
+  });
+});
