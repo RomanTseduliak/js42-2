d53cd4b37569090dca530febd012abe5c75c58d5
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2019-09-15 13:03:04 +0300

    Update day13.js

--- day13.js
+++ day13.js
@@ -73,3 +73,85 @@ describe('`Map` is a key/value map', function(){
     assert.equal(map.has(otherObj), false);
   });
 });
+//
+// 53: Map - initialize
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('initialize a `Map`', function(){
+  it('a `new Map()` is empty, has size=0', function() {
+    const mapSize = new Map().size;
+    assert.equal(mapSize, 0);
+  });
+  it('init Map with `[[]]` has a size=1', function() {
+    const mapSize = new Map().set('key', 'value').size;
+    assert.equal(mapSize, 1);
+  });
+  it('init a Map with `[[1]]` is the same as `map.set(1, void 0)`', function() {
+    let map1 = new Map([[1]]);
+    let map2 = new Map().set(1, void 0);
+    assertMapsEqual(map1, map2);
+  });
+  it('init Map with multiple key+value pairs', function() {
+    const pair1 = [1, 'one'];
+    const pair2 = [2, 'two'];
+    const map = new Map([pair1, pair2]);
+    assertMapsEqual(map, new Map().set(...pair1).set(...pair2));
+  });
+  it('keys are unique, the last one is used', function() {
+    const pair1 = [1, 'one'];
+    const pair2 = [1, 'one'];
+    const pair3 = [1, 'one'];
+    const pair4 = [2, 'two'];
+    const map = new Map([pair3, pair1, pair2, pair4]);
+    assertMapsEqual(map, new Map().set(...pair3).set(...pair4));
+  });
+  it('init Map from an Object, is a bit of work', function() {
+    let map = new Map();
+    const obj = {x: 1, y: 2};
+    const keys = Object.keys(obj);
+    keys.forEach(key => map.set(key, obj[key]));
+    const expectedEntries = [['x', 1], ['y', 2]];
+    assertMapsEqual(map, expectedEntries);
+  });
+});
+
+function mapToArray(map) {
+  return Array.from(map);
+}
+function assertMapsEqual(map1, map2) {
+  assert.deepEqual(mapToArray(map1), mapToArray(map2));
+}
+\\
+// 46: Map.prototype.set()
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Map.prototype.set` adds a new element with key and value to a Map', function(){
+  it('simplest use case is `set(key, value)` and `get(key)`', function() {
+    let map = new Map();
+    map.set('key', 'value');
+    assert.equal(map.get('key'), 'value');
+  });
+  it('the key can be a complex type too', function() {
+    const noop = function() {};
+    let map = new Map();
+    map.set(noop, 'the real noop');
+    assert.equal(map.get(noop), 'the real noop');
+  });
+  it('calling `set()` again with the same key replaces the value', function() {
+    let map = new Map();
+    map.set('key', 'value');
+    map.set('key', 'value1');
+    assert.equal(map.get('key'), 'value1');
+  });
+  it('`set()` returns the map object, it`s chainable', function() {
+    let map = new Map();
+    map.set(1, 'one')
+       .set(2, 'two')
+       .set(3, 'three')
+    ;
+    assert.deepEqual([...map.keys()], [1,2,3]);
+    assert.deepEqual([...map.values()], ['one', 'two', 'three']);
+  });
+});
